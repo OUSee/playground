73f82535cec944bc76200c693c7a9af95367f4ad
OUSee <666rld999@mail.ru>
2025-07-09 13:06:19 +0300

    auto deployment preparations

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
+  base: '/playground/',
   plugins: [vue()],
   resolve: {
     alias: {
