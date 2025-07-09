54a4eed47299e579fde34bc8cdd47825575c1625
OUSee <666rld999@mail.ru>
2025-07-09 14:01:44 +0300

    preset and check

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+
 
 </script>
 
 <template>
-
+  <HelloWorld />
 </template>
 
 <style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+  console.log(`=> abobana: ${'a'}`);
+  const count = 0;
+</script>
+
+<template>
+  <p>count</p>
+</template>
+
+<style scoped>
+    p{
+      color: red;
+    }
+</style>
